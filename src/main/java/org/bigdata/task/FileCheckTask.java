27e0af8da3cd8ed66db4f21294fb729ac4404208
KTE-060 <KTE-060@DESKTOP-TK3FLB1>
2022-10-24 10:49:49 +0900

    1

--- src/main/java/org/bigdata/task/FileCheckTask.java
+++ src/main/java/org/bigdata/task/FileCheckTask.java
@@ -10,6 +10,7 @@ import lombok.extern.log4j.Log4j;
 public class FileCheckTask {
 
 	//스케쥴링 선언
+	//매분 0초가 되었을때 checkFiles()메서드가 실행이 된다
 	@Scheduled(cron = "0 * * * * *")
 	public void checkFiles()throws Exception {
 		log.warn("run");
